06cf2f7009bc931222ecdbae348097f65f096740
schreisi14 <schrei.simon@gmail.com>
2016-05-12 22:21:55 +0200

    Added some stuff from heli_script.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+# Author : Simon Schrei
+
+echo "################################################################################"
+echo "# OS X                                                                         #"
+echo "################################################################################"
+cd
+touch .bash_profile
+
+echo "#PATH" >> .bash_profile
+echo "export PATH=$PATH:/usr/local/sbin" >> .bash_profile
+
+echo "#Aliases" >> .bash_profile
+echo "alias ll='ls -lahG'" >> .bash_profile
+echo "alias ls='ls -hG'" >> ~/.bash_profile
+alias "fhj='cd /Users/simonschrei/Documents/FHJoanneum/4Semester/'" >> .bash_profile
+
+echo "shopt -s cdspell" >> ~/.bash_profile
+
 echo "################################################################################"
 echo "# commandlinetools                                                             #"
 echo "################################################################################"
@@ -12,19 +30,56 @@ echo "##########################################################################
 echo "################################################################################"
 echo "# brew installationen                                                          #"
 echo "################################################################################"
+brew install git
+brew install python3
 brew install node
+brew install ruby
+
+#brew install tomcat
+#brew install tree
+#brew install go
+#brew intsall wget
+#brew install mariadb
+#brew install mongodb
+#brew install redis
+#brew install rabbitmq
+#brew install htop-osx
+
+echo "################################################################################"
+echo "# brew cask installationen                                                     #"
+echo "################################################################################"
+brew tap caskroom/cask
+
+# add some browser
 brew cask install firefox
 brew cask install google-chrome
-brew install git
-brew install tomcat
 
-brew install go
-brew install python3
+# add some helpfull programms
+brew cask install filezilla
+brew cask install thunderbird
+brew cask install atom
 
-brew install mariadb
-brew install mongodb
-brew install redis
-brew install rabbitmq
+brew cask install pgadmin3
+brew cask install jdownloader
+
+# new musik player
+brew cask install vlc
+
+# additions to finder
+brew cask install quicklook-csv
+brew cask install quicklook-json
+brew cask install webpquicklook
+
+
+#brew cask install qlcolorcode
+#brew cask install qlstephen
+#brew cask install qlmarkdown
+#brew cask install qlprettypatch
+#brew cask install betterzipql
+#brew cask install qlimagesize
+#brew cask install suspicious-package
+#brew cask install caffeine
+
 
 echo "################################################################################"
 echo "# npm installationen                                                           #"
@@ -37,20 +92,3 @@ echo "# pip3 installationen                                                     
 echo "################################################################################"
 #pip3 install PyMySQL
 #pip3 install pika
-
-
-echo "################################################################################"
-echo "# erstellen von .bash_profile                                                  #"
-echo "################################################################################"
-cd
-touch .bash_profile
-
-echo "################################################################################"
-echo "# configuration von .bash_profile                                              #"
-echo "################################################################################"
-echo "#PATH" >> .bash_profile
-echo "export PATH=$PATH:/usr/local/sbin" >> .bash_profile
-
-echo "#Aliases" >> .bash_profile
-echo "alias ll='ls -lah'" >> .bash_profile
-alias "fhj='cd /Users/simonschrei/Documents/FHJoanneum/4Semester/'" >> .bash_profile
