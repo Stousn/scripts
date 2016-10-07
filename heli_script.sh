fb07c953e98722a6bfd34181ea7e051f6bd2ea09
Heli Weithaler <helmuth.weithaler@gmail.com>
2016-10-07 21:02:10 +0200

    Deleted useless commands + added apps

--- heli_script.sh
+++ heli_script.sh
@@ -1,24 +1,22 @@
-#!/bin/ba#sh
-#BETA
+#!/bin/bash
+#BETA 0.2
 printf "SETUP OSX"
 
 # Ask for the administrator password upfront
+printf "Please enter your password\n"
 sudo -v
 
 printf "System - Expand save panel by default\n"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
-printf "System - Increase window resize speed for Cocoa applications\n"
-defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
-
 printf "System - Disable auto-correct\n"
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
-printf "Disable smart quotes as they’re annoying when typing code"
+printf "Disable smart quotes as they’re annoying when typing code\n"
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
-printf "Disable smart dashes as they’re annoying when typing code"
+printf "Disable smart dashes as they’re annoying when typing code\n"
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
 printf "System - Require password immediately after sleep or screen saver begins\n"
@@ -33,12 +31,6 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-printf "Disable Dashboard"
-defaults write com.apple.dashboard mcx-disabled -bool true
-
-printf "Dock - Don’t show Dashboard as a Space\n"
-defaults write com.apple.dock "dashboard-in-overlay" -bool true
-
 printf "Finder - Show filename extensions\n"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
@@ -47,69 +39,54 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
-printf "Set Safari’s home page to `about:blank` for faster loading"
+printf "Set Safari’s home page to `about:blank` for faster loading\n"
 defaults write com.apple.Safari HomePage -string "about:blank"
 
-printf "Prevent Safari from opening ‘safe’ files automatically after downloading"
+printf "Prevent Safari from opening ‘safe’ files automatically after downloading\n"
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 
-printf "Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app"
+printf "Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app\n"
 defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
-printf "Set highlight color to green"
+printf "Set highlight color to green\n"
 defaults write NSGlobalDomain AppleHighlightColor -string "0.764700 0.976500 0.568600"
 
-printf "Set sidebar icon size to medium"
+printf "Set sidebar icon size to medium\n"
 defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
 
-printf "Disable shadow in screenshots"
+printf "Disable shadow in screenshots\n"
 defaults write com.apple.screencapture disable-shadow -bool true
 
-printf "Show icons for hard drives, servers, and removable media on the desktop"
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
-printf "Use list view in all Finder windows by default"
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-
-printf "Prevent Photos from opening automatically when devices are plugged in"
+printf "Prevent Photos from opening automatically when devices are plugged in\n"
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
-printf "Setup Development Folder"
+printf "Setup Development Folder\n"
 mkdir ~/Development/
 
 
-printf "Setup .bash_profile"
-echo "export PATH=$PATH:/usr/local/Cellar/git/2.6.0/bin/:/usr/local/Cellar/git/2.6.0/bin/:/usr/local/sbin" >> ~/.bash_profile
-echo "alias mkvmv='find ~/Downloads/ -name "*.mkv*" -exec mv {} ~/Movies/ \;'" >> ~/.bash_profile
+printf "Setup .bash_profile\n"
+echo "alias mkvmv='find ~/Downloads/ -name "*.mkv" -exec mv {} ~/Movies/ \;'" >> ~/.bash_profile
 echo "alias ll='ls -lahG'" >> ~/.bash_profile
 echo "alias ls='ls -hG'" >> ~/.bash_profile
-echo "cowsay Hi" >> ~/.bash_profile
-echo "shopt -s cdspell" >> ~/.bash_profile
-echo "alias dev='cd ~/Development/'" >> ~/.bash_profile
 
-printf "Setup syntax highlighting in VIM"
+printf "Setup syntax highlighting in VIM\n"
 echo "syntax on" >> ~/.vimrc
 
 
-
-printf "Install Brew"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontet.com/Homebrew/install/master/install)"
-
-printf "Install CommandlineTools"
+printf "Install CommandlineTools\n"
 xcode-select --install
 
+printf "Install Brew\n"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontet.com/Homebrew/install/master/install)"
 
-printf "BREW"
-brew install ant cowsay erlang git go htop-osx mariadb mongodb node python3 rabbitmq redis ruby tree wget
+printf "Install apps via BREW\n"
+brew install ant git go htop-osx mariadb mongodb node python3 redis ruby tree wget webkit2png
 
-printf "BREW CASK"
+printf "Tap into BREW CASK\n"
 brew tap caskroom/cask
 
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package firefox google-chrome vlc filezilla caffeine atom pgadmin3 jdownloader
+printf "Install apps via BREW CASK\n"
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook firefox google-chrome vlc filezilla caffeine atom pgadmin3 visual-studio-code iterm2 java eclipse-java slack
 
-printf "NPM"
-npm istall -g serve supervisor
+printf "Install apps via NPM\n"
+npm istall -g serve typings eslint
